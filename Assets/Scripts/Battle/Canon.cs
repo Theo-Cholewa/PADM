48200b17429f91fdfe8e841d17b89287e22cddee
Jempasam <samueldemont83@gmail.com>
2025-10-20 17:11:56 +0200

    corrrection

--- Assets/Scripts/Battle/Canon.cs
+++ Assets/Scripts/Battle/Canon.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Canon : MonoBehaviour
