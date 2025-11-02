5acb7ff606f6e7766b7eb293f1293c04ee916ef1
Jempasam <samueldemont83@gmail.com>
2025-11-02 21:33:50 +0100

    Meilleur Controle des matelots aux doigts mouillés

--- Assets/Scripts/Battle/Flag.cs
+++ Assets/Scripts/Battle/Flag.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -29,48 +30,58 @@ public class Flag : MonoBehaviour
         transform.localScale /= 1.1f;
     }
 
-    private Pirate movingPirate = null;
+    private HashSet<Pirate> selectedPirates = new();
     private int gestSize = 0;
+
 
     void OnGestStart(Gestable gestable)
     {
-        gestSize = gestable.gestPointList.Count;
-
-        // Gest with one finger move the nearest pirate
-        if (gestSize == 1)
-        {
-            movingPirate = pirates
-                .Where(p => !p.IsDestroyed())
-                .OrderBy(p => (p.transform.position - transform.position).sqrMagnitude)
-                .FirstOrDefault();
-        }
     }
 
     void OnGestEnd(Gestable gestable)
     {
         gestSize = gestable.gestPointList.Count;
 
-        if (gestSize != 1)
+        if (gestSize == 0)
         {
-            movingPirate = null;
+            foreach (var pirate in pirates)
+            {
+                if (pirate.IsDestroyed()) continue;
+                pirate.SetHighlight(false);
+            }
+            selectedPirates.Clear();
         }
+
     }
 
     void OnGestUpdate(Gestable gestable)
     {
         var points = gestable.GetOrderedGestPoints().ToList();
 
-        // Move the single pirate
-        if (points.Count == 1 && movingPirate != null)
+        // Select the pirate pointed by the gesture
+        if (points.Count == 1)
         {
-            var direction = (points[0].position - transform.position);
-            movingPirate.Move(direction);
+            var direction = (points[0].position - transform.position).normalized;
+            foreach (var pirate in pirates)
+            {
+                if (pirate.IsDestroyed()) continue;
+                var distance = (transform.position - pirate.transform.position).magnitude;
+                var nearest_point = transform.position + direction * distance;
+                var distance_to_pirate = (nearest_point - pirate.transform.position).magnitude;
+                if (distance_to_pirate < 10f)
+                {
+                    if(selectedPirates.Add(pirate))
+                    {
+                        pirate.SetHighlight(true);
+                    }
+                }
+            }
         }
         // Move all pirates
         else if (points.Count == 2)
         {
-            var direction = ((points[0].position + points[1].position) / 2 - transform.position);
-            foreach (var pirate in pirates)
+            var direction = points[0].position - transform.position;
+            foreach (var pirate in selectedPirates)
             {
                 if (pirate.IsDestroyed()) continue;
                 pirate.Move(direction);
@@ -79,7 +90,7 @@ public class Flag : MonoBehaviour
         // Retrieve all pirates
         else if (points.Count == 3)
         {
-            foreach (var pirate in pirates)
+            foreach (var pirate in selectedPirates)
             {
                 if (pirate.IsDestroyed()) continue;
                 if((transform.position - pirate.transform.position).magnitude > HomeSize)
