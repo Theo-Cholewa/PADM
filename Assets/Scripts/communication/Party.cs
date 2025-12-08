0f2f8961dc6974a7c3ac70fc09bd097b0d3c20bf
Jempasam <samueldemont83@gmail.com>
2025-12-08 11:39:52 +0100

    remove test ip mode

--- Assets/Scripts/communication/Party.cs
+++ Assets/Scripts/communication/Party.cs
@@ -529,7 +529,7 @@ public class Party : MonoBehaviour
         // If editor
         if (Application.isEditor)
         {
-            return IPAddress.Parse("127.0.0.1");
+            //return IPAddress.Parse("127.0.0.1");
         }
 
         // Get from args
